d00a71ec725047baed4551a4693ecef6bf7af2cb
Taylor Wong <t105360507@ntut.edu.tw>
2020-10-05 14:15:37 +0800

    Updates

--- precache-manifest.cd75304be39fe6e8dbcea8cd3a7461b6.js
+++ precache-manifest.cd75304be39fe6e8dbcea8cd3a7461b6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0cc61511b666171a85832219ba113445",
+    "revision": "c83600dd20eb2356bfed4110f475bc96",
     "url": "/react-mqtt-ws-client/index.html"
   },
   {
-    "revision": "2120e8f3cd9dadbf6490",
+    "revision": "d8029eb63c29d46d1c0e",
     "url": "/react-mqtt-ws-client/static/css/main.c2dd9b8b.chunk.css"
   },
   {
-    "revision": "967543339fc8bc2f7399",
-    "url": "/react-mqtt-ws-client/static/js/2.f95c79dd.chunk.js"
+    "revision": "4fa9776aa96aa36db14f",
+    "url": "/react-mqtt-ws-client/static/js/2.bf50ec09.chunk.js"
   },
   {
     "revision": "1ab79f7743ae07e887277cb27f187c23",
-    "url": "/react-mqtt-ws-client/static/js/2.f95c79dd.chunk.js.LICENSE.txt"
+    "url": "/react-mqtt-ws-client/static/js/2.bf50ec09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2120e8f3cd9dadbf6490",
-    "url": "/react-mqtt-ws-client/static/js/main.b00e2e53.chunk.js"
+    "revision": "d8029eb63c29d46d1c0e",
+    "url": "/react-mqtt-ws-client/static/js/main.c7bc4232.chunk.js"
   },
   {
     "revision": "b5644244ea8f528bd114",
